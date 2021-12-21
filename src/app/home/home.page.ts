070662b4b62575f11aadfa2e0dbda4aa0e82343c
jd-f <alum.jesus.gailndo.zapata@ieslosremedios.org>
2021-12-21 11:22:22 +0100

    f

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,7 @@ import {map} from 'rxjs/operators';
 export class HomePage {
 
   users: any;
-
+  filtro: string = " ";
   constructor(private httpclient: HttpClient) {
     this.users = this.httpclient.get('https://raw.githubusercontent.com/jd-f/1.11T-Aplicaci-n-responsive-personalizada/main/src/MOCK_DATA.json');
   }
